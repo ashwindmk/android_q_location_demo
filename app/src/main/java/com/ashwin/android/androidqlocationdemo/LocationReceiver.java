db9ede772fece664b6aac81157fa09a947d220a2
Ashwin <ashwin.dinesh01@gmail.com>
2019-11-01 13:44:30 +0530

    Add manual location update option

--- app/src/main/java/com/ashwin/android/androidqlocationdemo/LocationReceiver.java
+++ app/src/main/java/com/ashwin/android/androidqlocationdemo/LocationReceiver.java
@@ -1,41 +1,99 @@
 package com.ashwin.android.androidqlocationdemo;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.location.Location;
+import android.os.Build;
+import android.text.TextUtils;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.location.LocationResult;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class LocationReceiver extends BroadcastReceiver {
-    private static final String TAG = "LUBroadcastReceiver";
-
+    private static final String TAG = "location-updates";
     static final String ACTION_PROCESS_UPDATES = "com.ashwin.locationdemo.action.PROCESS_UPDATES";
+    private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.US);
+    private static final String CHANNEL_ID = "channel_01";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_PROCESS_UPDATES.equals(action)) {
-                LocationResult result = LocationResult.extractResult(intent);
-                if (result != null) {
-                    List<Location> locations = result.getLocations();
-                    LocationResultHelper locationResultHelper = new LocationResultHelper(context, locations);
+            Log.w(TAG, "Received location update");
 
-                    // Save the location data to SharedPreferences.
-                    locationResultHelper.saveResults();
-
-                    // Show notification with the location data.
-                    locationResultHelper.showNotification();
-
-                    Log.i(TAG, LocationResultHelper.getSavedLocationResult(context));
-                } else {
-                    Log.e(TAG, "Location result is null");
-                }
+            if (ACTION_PROCESS_UPDATES.equals(intent.getAction())) {
+                String title = getLocationsTitle("Bg");
+                String message = getLocationsMessage(intent);
+                showNotification(context, title, message);
             }
         }
+    }
+
+    static String getLocationsTitle(String source) {
+        String timestamp = formatter.format(new Date());
+        return timestamp + ": " + source + " location update";
+    }
+
+    private String getLocationsMessage(Intent intent) {
+        LocationResult result = LocationResult.extractResult(intent);
+        if (result != null) {
+            List<Location> locations = result.getLocations();
+            if (locations != null) {
+                return getLocationsMessage(locations);
+            }
+        }
+        return "null";
+    }
+
+    static String getLocationsMessage(List<Location> locations) {
+        ArrayList<String> locationsMessageList = new ArrayList<>();
+        for (Location location : locations) {
+            String locationMessage = "(" + location.getLatitude() + ", " + location.getLongitude() + ")";
+            locationsMessageList.add(locationMessage);
+        }
+        return TextUtils.join(", ", locationsMessageList);
+    }
+
+    static void showNotification(Context context, String title, String message) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = context.getString(R.string.app_name);
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
+                .setColor(Color.RED)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setContentIntent(notificationPendingIntent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(CHANNEL_ID);
+        }
+
+        builder.setAutoCancel(true);
+
+        notificationManager.notify(title.hashCode(), builder.build());
     }
 }
--- app/src/main/java/com/ashwin/android/androidqlocationdemo/MainActivity.java
+++ app/src/main/java/com/ashwin/android/androidqlocationdemo/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -22,15 +23,23 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Q-Location";
     private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+    private static final long LOCATION_INTERVAL = 10_000L;
+    private static final long FASTEST_LOCATION_INTERVAL = LOCATION_INTERVAL / 2;
 
     private FusedLocationProviderClient fusedLocationClient;
     private LocationCallback locationCallback;
 
-    private Button coarseButton, fineButton, backgroundButton, startFgButton, startBgButton, stopFgButton, stopBgButton;
+    private Button coarseButton, fineButton, backgroundButton, updateNowButton, startFgButton, startBgButton, stopFgButton, stopBgButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,17 +52,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 if (locationResult == null) {
+                    Log.e(TAG, "Location result is null");
                     return;
                 }
-                for (Location location : locationResult.getLocations()) {
-                    Log.d(TAG, "location: (lat: " + location.getLatitude() + ", lng: " + location.getLongitude() + ")");
-                }
+
+                List<Location> locations = locationResult.getLocations();
+                LocationReceiver.showNotification(getApplicationContext(), LocationReceiver.getLocationsTitle("Fg"), LocationReceiver.getLocationsMessage(locations));
             }
         };
 
         coarseButton = (Button) findViewById(R.id.coarse_button);
         fineButton = (Button) findViewById(R.id.fine_button);
         backgroundButton = (Button) findViewById(R.id.background_button);
+        updateNowButton = (Button) findViewById(R.id.update_now_button);
         startFgButton = (Button) findViewById(R.id.start_fg_button);
         startBgButton = (Button) findViewById(R.id.start_bg_button);
         stopFgButton = (Button) findViewById(R.id.stop_fg_button);
@@ -62,14 +73,14 @@ public class MainActivity extends AppCompatActivity {
         coarseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startPermissionRequest(Manifest.permission.ACCESS_COARSE_LOCATION);
+                startPermissionRequest(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION});
             }
         });
 
         fineButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startPermissionRequest(Manifest.permission.ACCESS_FINE_LOCATION);
+                startPermissionRequest(new String[]{Manifest.permission.ACCESS_FINE_LOCATION});
             }
         });
 
@@ -77,10 +88,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    startPermissionRequest(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION});
+                    startPermissionRequest(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION});
                 } else {
                     Toast.makeText(MainActivity.this, "Android version is lower than 29 (Q)", Toast.LENGTH_LONG).show();
                 }
+            }
+        });
+
+        updateNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateLocation(getApplicationContext());
             }
         });
 
@@ -113,6 +131,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void updateLocation(final Context context) {
+        FusedLocationProviderClient fusedLocationProvider = LocationServices.getFusedLocationProviderClient(context);
+        fusedLocationProvider.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    Log.d(TAG, "Updated location: latitude: " + latitude + ", longitude: " + longitude);
+
+                    List<Location> locations = new ArrayList<>();
+                    locations.add(location);
+
+                    LocationReceiver.showNotification(context, LocationReceiver.getLocationsTitle("Manual"), LocationReceiver.getLocationsMessage(locations));
+                } else {
+                    Log.e(TAG, "Location is NULL");
+                }
+            }
+        });
+    }
+
     private PendingIntent getPendingIntent() {
         Intent intent = new Intent(this, LocationReceiver.class);
         intent.setAction(LocationReceiver.ACTION_PROCESS_UPDATES);
@@ -124,56 +163,113 @@ public class MainActivity extends AppCompatActivity {
         return permissionState == PackageManager.PERMISSION_GRANTED;
     }
 
-    private void startPermissionRequest(String permission) {
-        startPermissionRequest(new String[]{permission});
-    }
-
     private void startPermissionRequest(String[] permissions) {
         ActivityCompat.requestPermissions(MainActivity.this, permissions, REQUEST_PERMISSIONS_REQUEST_CODE);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        Log.i(TAG, "onRequestPermissionResult");
         if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
             if (grantResults.length <= 0) {
                 Log.i(TAG, "User interaction was cancelled.");
                 Toast.makeText(MainActivity.this, "Permission cancelled", Toast.LENGTH_LONG).show();
-            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.i(TAG, "Permission granted");
-                Toast.makeText(MainActivity.this, "Permission granted", Toast.LENGTH_LONG).show();
             } else {
-                Log.i(TAG, "Permission denied");
-                Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_LONG).show();
+                boolean allPermissionsGranted = false;
+
+                for (int grantResult : grantResults) {
+                    if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                        allPermissionsGranted = true;
+                    } else {
+                        allPermissionsGranted = false;
+                        break;
+                    }
+                }
+
+                if (allPermissionsGranted) {
+                    Log.i(TAG, "Permission granted");
+                    Toast.makeText(MainActivity.this, "Permission granted", Toast.LENGTH_LONG).show();
+                } else {
+                    Log.i(TAG, "Permission denied");
+                    Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_LONG).show();
+                }
             }
         }
     }
 
     private LocationRequest getLocationRequest() {
         LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setInterval(10_000);
-        locationRequest.setFastestInterval(5_000);
+        locationRequest.setInterval(LOCATION_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_LOCATION_INTERVAL);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         return locationRequest;
     }
 
     private void startFgLocationUpdates() {
         Log.i(TAG, "Starting fg location updates");
-        fusedLocationClient.requestLocationUpdates(getLocationRequest(), locationCallback, Looper.getMainLooper());
+        Task<Void> requestTask = fusedLocationClient.requestLocationUpdates(getLocationRequest(), locationCallback, Looper.getMainLooper());
+        requestTask.addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(Task<Void> task) {
+                if (task.isSuccessful()) {
+                    showToast("Fg location updates started successfully");
+                } else {
+                    showToast("Fg location updates start failed");
+                    showToast(String.valueOf(task.getException()));
+                }
+            }
+        });
     }
 
     private void startBgLocationUpdates() {
         Log.i(TAG, "Starting bg location updates");
-        fusedLocationClient.requestLocationUpdates(getLocationRequest(), getPendingIntent());
+        Task<Void> requestTask = fusedLocationClient.requestLocationUpdates(getLocationRequest(), getPendingIntent());
+        requestTask.addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(Task<Void> task) {
+                if (task.isSuccessful()) {
+                    showToast("Bg location updates started successfully");
+                } else {
+                    showToast("Bg location updates start failed");
+                    showToast(String.valueOf(task.getException()));
+                }
+            }
+        });
     }
 
     private void stopFgLocationUpdates() {
         Log.i(TAG, "Stopping fg location updates");
-        fusedLocationClient.removeLocationUpdates(locationCallback);
+        Task<Void> requestTask = fusedLocationClient.removeLocationUpdates(locationCallback);
+        requestTask.addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    showToast("Fg location updates stopped successfully");
+                } else {
+                    showToast("Fg location updates stop failed");
+                    showToast(String.valueOf(task.getException()));
+                }
+            }
+        });
     }
 
     private void stopBgLocationUpdates() {
         Log.i(TAG, "Stopping bg location updates");
-        fusedLocationClient.removeLocationUpdates(getPendingIntent());
+        Task<Void> requestTask = fusedLocationClient.removeLocationUpdates(getPendingIntent());
+        requestTask.addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    showToast("Bg location updates stopped successfully");
+                } else {
+                    showToast("Bg location updates stop failed");
+                    showToast(String.valueOf(task.getException()));
+                }
+            }
+        });
+    }
+
+    private void showToast(String msg) {
+        Log.d(TAG, msg);
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
 }
